456a42cf0b1cbe0a863060900bc5786370513404
Denbaiii <kkec228@gmail.com>
2024-02-07 18:11:41 +0600

    first commit

---
+++ db/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, Float, UniqueConstraint
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    price = Column(Float)
+    brand = Column(String)
+    discount = Column(Float)
+    rating = Column(Float)
+    volume = Column(String)
+    supplier_id = Column(Integer)
+    feedback_quantity = Column(Integer)
+    valuation = Column(String)
+
+    __table_args__ = (
+        UniqueConstraint("name", "price", name = "name_price_uc"),
+    )
---
+++ payment/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from .models import ShippingAddress
+
+class ShippingAddressForm(forms.ModelForm):
+    class Meta:
+        model = ShippingAddress
+        fields = ['full_name', 
+                  'email', 
+                  'street_address', 
+                  'apartment_address', 
+                  'country', 
+                  'zip',
+                  ]
+        exlude = ['user']
